3d0c7c7ae2d7c4b7d5ca6a89a7a80185a311d08d
kwakkyoleen <2007kkl@naver.com>
2023-11-08 16:41:16 +0900

    Update model page

--- front_code_ui_tts/src/component/modelComponent.jsx
+++ front_code_ui_tts/src/component/modelComponent.jsx
@@ -1,15 +1,30 @@
 import React from "react";
 import styles from "../css/model.module.css";
+import clipboardCopy from "clipboard-copy";
 
-const ModelComponent = ({modelid, description})=>{
+const ModelComponent = ({key, description})=>{
+
+    const handleCopyToClipboard = () => {
+        clipboardCopy(key);
+        alert("모델 id가 클립보드에 카피되었습니다.");
+    }
+
     return(
         <div className={styles.model}>
             <b className={styles.modelText}>{description}</b>
             <div className={styles.modelImgBox}>
-                <img className={styles.modelBtnImg} src="img/share.png"/>
-                <img className={styles.modelBtnImg} src="img/refresh.png"/>
-                <img className={styles.modelBtnImg} src="img/copy.png"/>
-                <img className={styles.modelBtnImg} src="img/delete.png"/>
+                <button>
+                    <img className={styles.modelBtnImg} src="img/share.png"/>
+                </button>
+                <button>
+                    <img className={styles.modelBtnImg} src="img/refresh.png"/>
+                </button>
+                <button onClick={handleCopyToClipboard} >
+                    <img className={styles.modelBtnImg} src="img/copy.png"/>
+                </button>
+                <button>
+                    <img className={styles.modelBtnImg} src="img/delete.png"/>
+                </button>
             </div>
         </div>
     );
