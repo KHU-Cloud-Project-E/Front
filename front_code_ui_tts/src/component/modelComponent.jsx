a4dfc728b3cdf6566841e25a94e7299cc965b932
kwakkyoleen <2007kkl@naver.com>
2023-11-08 16:23:36 +0900

    Add model button img

--- front_code_ui_tts/src/component/modelComponent.jsx
+++ front_code_ui_tts/src/component/modelComponent.jsx
@@ -5,6 +5,12 @@ const ModelComponent = ({modelid, description})=>{
     return(
         <div className={styles.model}>
             <b className={styles.modelText}>{description}</b>
+            <div className={styles.modelImgBox}>
+                <img className={styles.modelBtnImg} src="img/share.png"/>
+                <img className={styles.modelBtnImg} src="img/refresh.png"/>
+                <img className={styles.modelBtnImg} src="img/copy.png"/>
+                <img className={styles.modelBtnImg} src="img/delete.png"/>
+            </div>
         </div>
     );
 }
