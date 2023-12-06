c73760cb702279d753b8626b59c10ad58e29db0c
so3659 <so3659@naver.com>
2023-12-07 00:41:34 +0900

    Add Search & Copy

--- front_code_ui_tts/src/pages/gallery.jsx
+++ front_code_ui_tts/src/pages/gallery.jsx
@@ -3,67 +3,70 @@ import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import '../css/gallery.css';
 
-const baseUrl = import.meta.env.VITE_BACK_BASE_URL
+const baseUrl = import.meta.env.VITE_BACK_BASE_URL;
 
 function Gallery() {
   const [searchTerm, setSearchTerm] = useState('');
   const [images, setImages] = useState([]);
   const [page, setPage] = useState(1);
   const [isLoading, setIsLoading] = useState(false);
+  const [hasMore, setHasMore] = useState(true);
   const navigate = useNavigate();
 
   const limit = 9;
 
-  const fetchImages = async () => {
+  const fetchModels = async (lastId = 0, name = '') => {
     setIsLoading(true);
-  
-
-    const fetchModels = async (lastId = 0, name = '') => {
-      try {
-        const response = await axios.get(`${baseUrl}/models`, {
-          params: { lastId, name }
-        });
-        return response.data.result.listUserModelDetailDto;
-      } catch (error) {
-        console.error('Error at fetching models:', error);
-        return [];
-      }
-    };
-  
     try {
-      const modelDetails = await fetchModels(page * limit - limit, searchTerm);
-  
-
-      const newImages = modelDetails.map((model) => ({
-        id: model.model_id,
-        src: model.image_url,
-        description: model.description || 'No description',
-      }));
-  
-      setImages(prev => [...prev, ...newImages]);
+      const response = await axios.get(`${baseUrl}/models`, {
+        params: { lastId, name }
+      });
+      setIsLoading(false);
+      return response.data.result.listUserModelDetailDto;
     } catch (error) {
-      console.error('Error while fetching images:', error);
+      console.error('Error at fetching models:', error);
+      setIsLoading(false);
+      return [];
     }
-  
-    setIsLoading(false);
   };
-  
+
+  const fetchImages = async () => {
+    const modelDetails = await fetchModels(page * limit - limit, searchTerm);
+    
+    setHasMore(modelDetails.length === limit);
+
+    const newImages = modelDetails.map(model => ({
+      id: model.model_id,
+      src: model.image_url,
+      description: model.description || 'No description',
+    }));
+
+    setImages(prev => [...prev, ...newImages]);
+  };
+
   useEffect(() => {
+    setPage(1);
+    setImages([]);
+    setHasMore(true);
     fetchImages();
+  }, [searchTerm]);
+
+  useEffect(() => {
+    if (page > 1) fetchImages();
   }, [page]);
 
   useEffect(() => {
     const handleScroll = () => {
-      if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 100 && !isLoading) {
+      if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 100 && !isLoading && hasMore) {
         setPage(prev => prev + 1);
       }
     };
 
     window.addEventListener('scroll', handleScroll);
     return () => window.removeEventListener('scroll', handleScroll);
-  }, [isLoading]);
+  }, [isLoading, hasMore]);
 
-  const handleImageClick = (component) => {
+  const handleImageClick = component => {
     navigate(`/detail/${component.id}`, { state: { imageSrc: component.src } });
   };
 
@@ -75,11 +78,11 @@ function Gallery() {
           type="text"
           placeholder="Search Model..."
           value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
+          onChange={e => setSearchTerm(e.target.value)}
         />
       </div>
       <div className="image-grid">
-        {images.map((component) => (
+        {images.map(component => (
           <div key={component.id} className="image-card" onClick={() => handleImageClick(component)}>
             <img src={component.src} alt={`Image ${component.id}`} className="model-image" />
             <div className="description">{component.description}</div>
