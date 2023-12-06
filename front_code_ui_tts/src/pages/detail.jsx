a492b08bdbe4d834ec2b233df9335c8fe14f7bbf
kwakkyoleen <2007kkl@naver.com>
2023-12-06 22:40:27 +0900

    Revert detail

--- front_code_ui_tts/src/pages/detail.jsx
+++ front_code_ui_tts/src/pages/detail.jsx
@@ -1,18 +1,40 @@
-import React from "react";
-import { useParams, useNavigate, useLocation } from "react-router-dom";
-import "../css/detail.css";
+import React, { useState, useEffect } from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
+import axios from 'axios'; 
+import '../css/detail.css';
+
+const baseUrl = "api"; // API의 기본 URL을 설정
 
 const Detail = () => {
   let { id } = useParams();
-  let location = useLocation();
   let navigate = useNavigate();
-  const imageSrc = location.state?.imageSrc;
+  const [imageSrc, setImageSrc] = useState('');
+  const [details, setDetails] = useState({
+    title: "Loading...",
+    text: [],
+  });
 
-  const details = {
-    title: "Title",
-    text: Array(10).fill("Details"),
-    imageSrc: imageSrc,
+  // 이미지와 상세 정보를 불러오는 함수
+  const fetchDetails = async () => {
+    try {
+      const response = await axios.get(`${baseUrl}/model-details/${id}`);
+      if (response.status === 200) {
+        setDetails({
+          title: response.data.title,
+          text: response.data.text,
+          imageSrc: response.data.imageSrc,
+        });
+        setImageSrc(response.data.imageSrc);
+      }
+    } catch (error) {
+      console.error('Error fetching details:', error);
+    }
   };
+
+  // 컴포넌트가 마운트될 때 API 호출
+  useEffect(() => {
+    fetchDetails();
+  }, [id]);
 
   // 뒤로가기 함수
   const goBack = () => {
@@ -22,15 +44,13 @@ const Detail = () => {
   return (
     <div className="detail-container">
       <h1>{details.title}</h1>
-      <img className="detail-image" src={imageSrc} />
+      {imageSrc ? <img className="detail-image" src={imageSrc} alt={details.title} /> : <p>Loading image...</p>}
       <div className="detail-info">
         {details.text.map((detail, index) => (
           <p key={index}>{detail}</p>
         ))}
       </div>
-      <button onClick={goBack} className="back-button">
-        Back
-      </button>
+      <button onClick={goBack} className="뒤로가기">Back</button>
     </div>
   );
 };
