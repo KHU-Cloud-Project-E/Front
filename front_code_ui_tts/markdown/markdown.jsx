63b33b66e239bc40e12cb5049eb1ea870ec7b4d9
kwakkyoleen <2007kkl@naver.com>
2023-12-10 04:38:24 +0900

    Update help page

--- front_code_ui_tts/markdown/markdown.jsx
+++ front_code_ui_tts/markdown/markdown.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactMarkdown from "react-markdown";
 import remarkGfm from 'remark-gfm'
+import "./github-markdown-light.min.css";
 
 const MarkdownComp = ({ markdownContent }) => {
-  return <ReactMarkdown className={"mark"} remarkPlugins={[remarkGfm]}>{markdownContent}</ReactMarkdown>;
+  return <ReactMarkdown className={"markdown-body"} remarkPlugins={[remarkGfm]}>{markdownContent}</ReactMarkdown>;
 };
 
 export default MarkdownComp;
